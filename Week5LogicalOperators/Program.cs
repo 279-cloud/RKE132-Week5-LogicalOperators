e71cc20f0e1610ffc32123289c444f80dc2dd945
Svetlana Keersalu <svetik24@hotmail.com>
2023-02-25 21:12:27 +0200

    add week5 code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,28 @@
+﻿//AND - &&
+
+//true && true --> true
+//true && false --> false
+//false && true --> false
+//false && false --> false
+
+
+//OR - || (pipes)
+
+//true || true --> true
+//true || false --> true
+//false || true --> true
+//false || false --> false
+
+
+string userPIN, userName, password;
+Console.WriteLine("Enter PIN:");
+userPIN = Console.ReadLine();
+
+if(userPIN == "1234")
+{
+else
+    {
+        Console.WriteLine("Invalid PIN");
+
+
+
